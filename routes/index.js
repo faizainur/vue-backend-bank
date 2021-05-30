d4ac8513de99e55ad5b420752a87bd068925b71f
Faiz Ainur Rofiq <cavadevop@gmail.com>
2021-05-30 15:51:14 +0700

    add storing refreshToken to DB mechanism

--- routes/index.js
+++ routes/index.js
@@ -2,49 +2,49 @@ const { json, query } = require("express");
 var express = require("express");
 var router = express.Router();
 const url = require("url");
+const mongoose = require("mongoose");
+const jwt_decode = require("jwt-decode");
+const querystring = require("querystring");
 var passport = require("passport"),
   OAuth2Strategy = require("passport-oauth").OAuth2Strategy;
+const MongoClient = require("mongodb").MongoClient;
 
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const CLIENT_ID = process.env.CLIENT_ID;
 const WEB_CLIENT_URL = process.env.WEB_CLIENT_URL;
-const jwt_decode = require("jwt-decode");
-const querystring = require("querystring");
+const BANK_NAME = process.env.BANK_NAME;
+const MONGODB_URI = process.env.MONGODB_URI;
+const CALLBACK_URL = process.env.CALLBACK_URL;
 
-passport.use(
-  "provider",
-  new OAuth2Strategy(
-    {
-      authorizationURL: "https://hydra.catena.id/client/oauth2/auth",
-      tokenURL: "https://hydra.catena.id/client/oauth2/token",
-      // clientID: "d676e5d0-93e2-44c1-a9e8-adbb7c40544b",
-      clientID: CLIENT_ID,
-      // clientSecret: "KHmr_OrR1JStN0UmHXaYJLJLMy",
-      clientSecret: CLIENT_SECRET,
-      callbackURL: "http://localhost:4000/callback",
-      scope: ["profile", "openid", "offline", "ru_financial_data"],
-    },
-    function (accessToken, refreshToken, result, profile, done) {
-      // console.log(req);
-      // console.log(accessToken);
-      // console.log(refreshToken);
-      // // console.log(profile);
-      // console.log(result);
-      var idToken = result.id_token;
-      var decoded = jwt_decode(idToken);
-      console.log(decoded.sub);
-      if (accessToken && refreshToken) {
-        return done(null, true, {
-          accessToken: accessToken,
-          refreshToken: refreshToken,
-          email: decoded.sub,
-        });
-      } else {
-        return done(null, false);
-      }
+const strategy = new OAuth2Strategy(
+  {
+    authorizationURL: "https://hydra.catena.id/client/oauth2/auth",
+    tokenURL: "https://hydra.catena.id/client/oauth2/token",
+    // clientID: "d676e5d0-93e2-44c1-a9e8-adbb7c40544b",
+    clientID: CLIENT_ID,
+    // clientSecret: "KHmr_OrR1JStN0UmHXaYJLJLMy",
+    clientSecret: CLIENT_SECRET,
+    callbackURL: CALLBACK_URL,
+    scope: ["profile", "openid", "offline", "ru_financial_data"],
+  },
+
+  function (accessToken, refreshToken, result, profile, done) {
+    var idToken = result.id_token;
+    var decoded = jwt_decode(idToken);
+    console.log(decoded.sub);
+    if (accessToken && refreshToken) {
+      return done(null, true, {
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+        email: decoded.sub,
+      });
+    } else {
+      return done(null, false);
     }
-  )
+  }
 );
+
+passport.use("provider", strategy);
 
 router.get("/", function (req, res, next) {
   res.send("login");
@@ -58,27 +58,82 @@ router.get(
   })
 );
 
-router.get("/callback", (req, res, next) => {
-  passport.authenticate(
-    "provider",
-    {
-      state: "stateAdevcatena",
-      session: false,
-    },
-    (err, user, info) => {
-      if (user) {
-        console.log(WEB_CLIENT_URL);
-        const query = querystring.stringify({
-          email: info.email,
-          accessToken: info.accessToken,
-          refreshToken: info.refreshToken,
-        });
-        return res.redirect(WEB_CLIENT_URL + "?" + query);
-      } else {
-        return res.send("Unauthorized").status(401);
-      }
-    }
-  )(req, res, next);
-});
+MongoClient.connect(
+  MONGODB_URI,
+  {
+    useUnifiedTopology: true,
+  },
+  async (err, client) => {
+    if (err) return console.error(err);
+    console.log("Connected to Database");
+
+    const db = client.db("oauth2");
+    const collection = db.collection("authorized");
+
+    router.get("/callback", (req, res, next) => {
+      passport.authenticate(
+        "provider",
+        {
+          state: "stateAdevcatena",
+          session: false,
+        },
+        (err, user, info) => {
+          if (user) {
+            collection
+              .findOne({
+                email: info.email,
+                bankName: BANK_NAME,
+              })
+              .then((data) => {
+                if (data) {
+                  collection
+                    .findOneAndUpdate(
+                      {
+                        email: info.email,
+                        bankName: BANK_NAME,
+                      },
+                      {
+                        $set: {
+                          email: info.email,
+                          bankName: BANK_NAME,
+                          refreshToken: info.refreshToken,
+                        },
+                      }
+                    )
+                    .then((result) => {
+                      const query = querystring.stringify({
+                        email: info.email,
+                        accessToken: info.accessToken,
+                        refreshToken: info.refreshToken,
+                      });
+                      return res.redirect(WEB_CLIENT_URL + "?" + query);
+                    })
+                    .catch((error) => res.send(error));
+                } else {
+                  collection
+                    .insertOne({
+                      email: info.email,
+                      bankName: BANK_NAME,
+                      refreshToken: info.refreshToken,
+                    })
+                    .then((data) => {
+                      const query = querystring.stringify({
+                        email: info.email,
+                        accessToken: info.accessToken,
+                        refreshToken: info.refreshToken,
+                      });
+                      return res.redirect(WEB_CLIENT_URL + "?" + query);
+                    })
+                    .catch((error) => res.send(error));
+                }
+              });
+          } else {
+            return res.send("Unauthorized").status(401);
+          }
+        }
+      )(req, res, next);
+    });
+  }
+);
 
 module.exports = router;
